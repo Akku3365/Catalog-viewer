0640289b497516b087667ea700cbb4b424ff938f
Akash Srivastava <akashshrivastava00952@gmail.com>
2023-05-31 18:47:19 +0530

    fixed UI

--- src/CatalogViewer.js
+++ src/CatalogViewer.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Box, Grid, IconButton } from '@mui/material';
+import { Box, IconButton } from '@mui/material';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
@@ -60,20 +60,54 @@ const CatalogViewer = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Box style={{height: "94vh" }}>
-        <Grid container>
-          <Grid style={{ display: "flex", flexDirection: "column" }}>
-            <Box style={{ display: "flex",marginTop: "25px", height: "70vh",width: "98vw" }}>
-              <img src={currentImage.url} alt={currentImage.details} style={{ width: "42%", maxHeight: '100%', borderRadius: "35px", marginLeft: "32px" }} />
-              <Box  style={{textAlign: "left",font: "normal normal normal 16px/18px Ubuntu",letterSpacing:    "0px",color: "#B4B3B3",opacity: "1", marginLeft: "65px", width: "40vw"}}>
-                <h1 style={{fontFamily: "normal normal normal 50px/57px Ubuntu", color: "black"}}>W Maldivs</h1>
+            <Box style={{ display: "flex",marginTop: "25px", height: "1080px",width: "1920px" }}>
+                <Box>
+              <img src={currentImage.url} alt={currentImage.details} style={{ width: "1126px", height: '768px', borderRadius: "35px", marginLeft: "32px", marginRight: "10px"}} />
+
+
+              <Box style={{ width: "1126px", height: "171px", marginLeft: "32px", marginTop: "30px" }}>
+             <IconButton onClick={handlePreviousImage} >
+                <ArrowBackIcon />
+              </IconButton>
+              {images.map((image) => (
+                <IconButton key={image.id} onClick={() => handleThumbnailClick(image)}>
+                  <img
+                    src={image.url}
+                    alt={image.details}
+                    style={{
+                      width: 190,
+                      height: 145,
+                      borderRadius: 18,
+                      filter: image.id === currentImage.id ? 'none' : 'grayscale(100%)',
+                    }}
+                  />
+                </IconButton>
+              ))}
+              <IconButton onClick={handleNextImage}>
+                <ArrowForwardIcon />
+              </IconButton>
+            </Box>
+              </Box>
+              <Box style={{width: "620px", height: "933px",  marginLeft: "35px",}} >
+                <h1 style={{fontFamily: "normal normal normal 50px/57px Ubuntu", color: "grey"}}>W Maldivs</h1>
+                <Box  style={{textAlign: "left",font: "normal normal normal 16px/18px Ubuntu",letterSpacing:    "0px",color: "#B4B3B3",opacity: "1", width: "590px",height: "540px"}}>
               <p>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
               </p>
               </Box>
+              <IconButton onClick={handleSlideshowToggle} style={{ marginTop: "190px", marginLeft: "50%"}}>
+                {isSlideshowActive ? (
+                  <PauseCircleOutlineIcon style={{background: "#25BEDA 0% 0% no-repeat padding-box",
+                    opacity: "1", fontSize: "90px", borderRadius: "50px"}}/>
+                ) : (
+                  <PlayCircleOutlineIcon style={{background: "#25BEDA 0% 0% no-repeat padding-box",
+                  opacity: "1", fontSize: "90px", borderRadius: "50px"}}/>
+                )}
+              </IconButton>
+              </Box>
             </Box>
-            <Box style={{ display: "flex", marginTop: "10px", width: "98vw" }}>
-            <IconButton onClick={handlePreviousImage} >
+            {/* <Box style={{ border: "2px solid red",width: "905px", height: "171px" }}>
+             <IconButton onClick={handlePreviousImage} >
                 <ArrowBackIcon />
               </IconButton>
               {images.map((image) => (
@@ -93,21 +127,7 @@ const CatalogViewer = () => {
               <IconButton onClick={handleNextImage}>
                 <ArrowForwardIcon />
               </IconButton>
-              <Box style={{ display: "flex", marginTop: "10px", marginLeft: "22vw", height: "15vh" }}>
-              <IconButton onClick={handleSlideshowToggle}>
-                {isSlideshowActive ? (
-                  <PauseCircleOutlineIcon style={{background: "#25BEDA 0% 0% no-repeat padding-box",
-                    opacity: "1", fontSize: "90px", borderRadius: "50px"}}/>
-                ) : (
-                  <PlayCircleOutlineIcon style={{background: "#25BEDA 0% 0% no-repeat padding-box",
-                  opacity: "1", fontSize: "90px", borderRadius: "50px"}}/>
-                )}
-              </IconButton>
-             </Box>
-            </Box>
-          </Grid>
-        </Grid>
-      </Box>
+            </Box> */}
     </ThemeProvider>
   );
 };
